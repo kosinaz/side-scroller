bd871c24fa9f083e4153024d0279d7b7e17ddc4e
Kosina Zoltán <kosinaz@gmail.com>
2018-09-04 08:57:47 +0200

    Added player, physics and controls

--- src/play.js
+++ src/play.js
@@ -1,10 +1,36 @@
 var playState = {
 
     create: function () {
+
+        /**
+         * Set the player.
+         */
+        game.player = game.add.sprite(924, 288, 'sprites', 'sprite.png');
+        game.player.anchor = {x: 0.5, y: 0.5};
         
+        /**
+         * Set the gravity.
+         */
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 1000;
+        game.physics.enable(game.player, Phaser.Physics.ARCADE);
+        game.player.body.collideWorldBounds = true;
+
+        /**
+         * Set the controls.
+         */
+        game.input.mouse.capture = true;
+
     },
 
     update: function () {  
+
+        /**
+         * Rise the player.
+         */
+        if (game.input.activePointer.leftButton.isDown) {
+            game.player.body.velocity.y = -500;
+        }
 
     }
 
