2280dce491ade184158998f1b26df64bcbb6150c
Kosina Zoltán <kosinaz@gmail.com>
2018-09-10 22:09:30 +0200

    Adjusted score label position

--- src/scoreLabel.js
+++ src/scoreLabel.js
@@ -1,6 +1,6 @@
 function createScoreLabel() {
   game.levelScore = game.totalScore;
-  game.levelScoreLabel = game.add.text(450, 50, 'Score: ' + game.levelScore, game.style);
+  game.levelScoreLabel = game.add.text(512, 50, 'Score: ' + game.levelScore, game.style);
 }
 function updateScoreLabel() {
   game.levelScoreLabel.text = 'Score: ' + game.levelScore;
