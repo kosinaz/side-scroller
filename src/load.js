466c61d7041cc093ae000a5009f0329bcce937e6
Kosina Zoltán <kosinaz@gmail.com>
2018-09-10 23:31:34 +0200

    Added difficulty level-specific music

--- src/load.js
+++ src/load.js
@@ -10,7 +10,9 @@ var loadState = {
     /**
      * Load the music.
      */
-    game.load.audio('music', ['assets/music.ogg', 'assets/music.m4a']);
+    game.load.audio('easy', ['assets/easy.ogg', 'assets/easy.m4a']);
+    game.load.audio('medium', ['assets/medium.ogg', 'assets/medium.m4a']);
+    game.load.audio('hard', ['assets/hard.ogg', 'assets/hard.m4a']);
   },
 
   create: function () {
@@ -18,7 +20,7 @@ var loadState = {
     /**
      * Start the music.
      */
-    game.music = game.add.audio('music', 0.3, true).play();
+    game.music = game.add.audio('easy', 0.3, true).play();
 
     loadPlayer(function () {
       game.state.start('menu');
